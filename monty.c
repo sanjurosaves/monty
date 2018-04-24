df80be2b87d36ef22b443f80f3320793ccf14fbe
AmyLGalles <suzyhendrix@gmail.com>
2018-04-24 19:27:58 +0000

    pseudocode main file

---
+++ monty.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+int main(int argc char *argv[])
+{
+	int fd;
+
+	if (argc != 2)
+	{
+		printf("USAGE: monty file");
+		exit(EXIT_FAILURE);
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		printf("Error: Can't open file %s", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+/** if (instructions in file are not in header)*/
+/**{
+/**printf("L%d: unknown instruction %s", <line number> <opcode>);*/
+/**exit(EXIT FAILURE);
+/**}
+}
