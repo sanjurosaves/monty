c20cc87405b5e351cd9fb2a808fcb702ba5c4ccf
Alex Allen <325@holbertonschool.com>
2018-04-24 22:26:25 +0000

    implementing fopen/getline

--- monty.c
+++ monty.c
@@ -1,7 +1,10 @@
 #include "monty.h"
 int main(int argc, char *argv[])
 {
-	int fd;
+	int fd, char_count;
+	char *buf;
+	size_t buf_size = 0;
+	FILE* fs;
 
 	if (argc != 2)
 	{
@@ -10,12 +13,24 @@ int main(int argc, char *argv[])
 	}
 
 	fd = open(argv[1], O_RDONLY);
-
 	if (fd == -1)
 	{
 		printf("Error: Can't open file %s", argv[1]);
 		exit(EXIT_FAILURE);
 	}
+	close(fd);
+
+	fs = fopen(argv[1], "r");
+	char_count = getline(&buf, &buf_size, fs);
+	if (char_count == -1)
+	{
+		free (buf);
+		printf("Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+/*	close(fs); */
+
+	printf("%s\n", buf);
 
 	return (0);
 
