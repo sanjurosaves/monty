aff74590860948688940fb865bb08a850a648dd6
AmyLGalles <suzyhendrix@gmail.com>
2018-04-25 21:30:58 +0000

    added new line to error messages

--- monty.c
+++ monty.c
@@ -24,14 +24,14 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-		printf("USAGE: monty file");
+		printf("USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 	{
-		printf("Error: Can't open file %s", argv[1]);
+		printf("Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	close(fd);
