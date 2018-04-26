47e06523404f8ccd05d316aa3a32f4ab2ae0ad4f
AmyLGalles <suzyhendrix@gmail.com>
2018-04-26 21:28:47 +0000

    removed null check

--- pop.c
+++ pop.c
@@ -9,13 +9,7 @@ void pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
 	stack_t *temp2;
-/**
- *	if (*stack == NULL)
- *	{
- *		printf("L%u: can't pop an empty stack\n", line_number);
- *		exit(EXIT_FAILURE);
- *	}
- */
+
 	temp = *stack;
 	temp2 = temp->next;
 	free(temp);
