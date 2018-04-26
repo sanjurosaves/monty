3cd30d569949b4d5057e54f48f879b6dc1e03f35
Alex Allen <325@holbertonschool.com>
2018-04-26 17:50:13 +0000

    add sub

---
+++ sub.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+/**
+ * add - adds data from top two stack elements
+ * @stack: pointer to the head of the list
+ *@line_number: current line number in main for error handling
+ *Return: none/void
+ *
+ */
+void sub(stack_t **stack, unsigned int line_number)
+{
+	int length, diff = 0;
+        stack_t *temp;
+
+        temp = *stack;
+	length = stack_len(*stack, line_number);
+	if (length < 2)
+	{
+		exit(EXIT_FAILURE);
+	}
+	diff += ((-1 * temp->n) + temp->next->n);
+	pop(stack, line_number);
+	(*stack)->n = diff;
+        return;
+}
