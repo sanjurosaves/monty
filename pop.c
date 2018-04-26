348a0c1b500893b50e7cabd343fca7fb6b831b54
AmyLGalles <suzyhendrix@gmail.com>
2018-04-26 21:35:22 +0000

    wasn't that edge case

--- pop.c
+++ pop.c
@@ -9,10 +9,7 @@ void pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
 	stack_t *temp2;
-	while (temp->prev != NULL)
-	{
-		temp = temp->prev;
-	}
+
 	if (*stack == NULL)
 	{
 		printf("L%u: can't pop an empty stack\n", line_number);
