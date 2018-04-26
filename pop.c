faafeb0a35db9b1a933d30b8b1e582b6d9b141f6
AmyLGalles <suzyhendrix@gmail.com>
2018-04-26 21:26:59 +0000

    testing pop output

--- pop.c
+++ pop.c
@@ -12,7 +12,7 @@ void pop(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL)
 	{
-		printf("L%u: can't pop an empty stack\n", line_number);
+		fprintf(stdout, "L%u: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
